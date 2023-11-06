56d399f73ee42ad302785d65db226ec49618d080
Yousuk-Song <71381603+Yousuk-Song@users.noreply.github.com>
2023-11-07 02:18:07 +0900

    Update run_analysis.py

--- run_analysis.py
+++ run_analysis.py
@@ -1,29 +1,17 @@
-#############
-#2021.08.27
-#.gz/.fastq -> .gz만 돌아가게수정
-#원본은 .fastq를 위한건데 잘 돌아갔음
-#.gz들어가니까 if문 for문이 추가되는데 좀 번거로운 것 같기도 하고 앞으로 gz만 써야 용량 관리할 때 효율적일 것 같아서 gz만 되는 걸로 바꿈
-##############
+
 import os 
 import sys
-#from contextlib import contextmanager 필요없었던 것 같음 
 import zipfile
 
-#초기설정 
-#ex)E010T_1.fastq, E010T2_1.fastq .... (gz만 돌아가게 수정 )
-#이때 꼭 1과2의 순서를 맞춰서 입력해야한다. 
 fastq1=list(map(str,sys.argv[1].strip().split(','))) 
 fastq2=list(map(str,sys.argv[2].strip().split(',')))
 
-#project별로 dir가 생성되기 때문에 참고해서 진행한다 
 project_name= sys.argv[3]
 
-#돌리기전에 rsem,star에 대한 index 파일은 이미 생성돼 있어야한다
-#star는 ref밑에 있고 rsem은 따로있음 
-wdir='/data/juyoung/bulkRNAseq/'
-genome_ref_dir = '/data/juyoung/ref/' 
-rsem_ref_dir='/data/juyoung/rsem/'
-raw_dir='/data/juyoung/rawdata/'
+wdir=''
+genome_ref_dir = '' 
+rsem_ref_dir=''
+raw_dir=''
 
 #fastaq 파일이 match되지 않을 때 - > 시스템종료 
 if (len(fastq1) != len(fastq2)):
@@ -35,7 +23,7 @@ for i in range(len(fastq1)):
         fastq2[i] = fastq2[i][:-3]
         
 #make directory
-#trimming 전후로 qc를 체크함 
+#trimming 전후로 qc를 체크
 os.chdir(wdir)
 os.makedirs('./'+project_name,exist_ok=True)
 os.chdir('./'+project_name)
@@ -53,7 +41,7 @@ for i in range(len(fastq1)):
 os.chdir('./qc')#easy to move os.chdir command 
 cdir = os.getcwd()
 
-#fastqc 결과가 모여있는 폴더에서 _fastqc.zip파일을 열고 그 안에서 summary.txt에 해당하는 파일을 연다. 그 후 안에 있는 내용들을 모아 하나의 파일로 정리한다
+#fastqc 결과가 모여있는 폴더에서 _fastqc.zip파일을 열고 그 안에서 summary.txt에 해당하는 파일을 연다. 그 후 안에 있는 내용들을 모아 하나의 파일로 정리
 files = [file for file in os.listdir(cdir) if file.endswith('_fastqc.zip')]
 all_summary = []
 
